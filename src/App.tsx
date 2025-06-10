245b5c397b460b3b1d7d87819e036b77c9f9cd6b
Dinesh Sandaruwan <imdineshsandaru@gmail.com>
2025-06-10 12:45:34 +0530

    Update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { LMSProvider } from './contexts/LMSContext';
 import Navbar from './components/Navbar';
